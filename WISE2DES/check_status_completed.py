9017a6666623ab338fc50d3a677c73b775f34e9e
richardgmcmahon <rgm@ast.cam.ac.uk>
2016-06-27 12:54:07 +0100

    modified:   check_status_completed.py

--- WISE2DES/check_status_completed.py
+++ WISE2DES/check_status_completed.py
@@ -1,6 +1,73 @@
 from __future__ import print_function, division
 
+"""
+
+function to check output products by DES tile subdirectory.
+
+loosely based on astropy fitscheck.py
+
+to get help type:
+python check_status_completed.py --help
+
+
+"""
+
 from astropy.io import fits
+
+
+def check_tile(inpath=None, debug=False, fast=False, count=False,
+    ncheckfiles=None, clean=False):
+    """
+
+
+    """
+    import shutil
+
+    if debug: print('Processing:', inpath)
+    debug = False
+    filelist = glob.glob(inpath + '/*fit*')
+    nfiles = len(filelist)
+
+    filelist_all = glob.glob(inpath)
+    nfiles_all = len(filelist)
+
+    print('Number of fits files out of all:', nfiles, nfiles_all)
+
+    # verify all fits files
+    if not fast:
+        for infile in filelist:
+            if debug: print(infile)
+
+            hdulist = fits.open(infile)
+            nhdu = len(hdulist)
+            if debug: print('Number of extension:', nhdu)
+
+            for ihdu in xrange(0, nhdu):
+                if hdulist[ihdu].verify() is not None:
+                   print(ihdu, hdulist[ihdu].verify())
+
+    if fast and clean and nfiles != ncheckfiles:
+        reply = raw_input("Delete directory "+ inpath +"? [y/[n]] ")
+        if reply == 'y' or reply == 'Y':
+            shutil.rmtree(inpath)
+
+        filelist = glob.glob(inpath + '/*fit*')
+        nfiles = len(filelist)
+        print('Number of fits files remaining:', nfiles)
+
+    if fast and not count and nfiles != ncheckfiles:
+        for infile in filelist:
+            if debug: print(infile)
+
+            hdulist = fits.open(infile)
+            nhdu = len(hdulist)
+            if debug: print('Number of extension:', nhdu)
+
+            for ihdu in xrange(0, nhdu):
+                if hdulist[ihdu].verify() is not None:
+                   print(ihdu, hdulist[ihdu].verify())
+
+    return nfiles
 
 
 def check_status_completed(tilename=None, datapath=None):
@@ -12,16 +79,12 @@ def check_status_completed(tilename=None, datapath=None):
     could also verify that DES2327-5248_WISEfp_DEScat_WISE_match.fits
     is valid FITS file
 
-
     http://docs.astropy.org/en/stable/io/fits/usage/verification.html#verification-using-the-fits-checksum-keyword-convention
 
 
     see also: https://github.com/astropy/astropy/tree/master/astropy/io/fits/scripts
 
     fitscheck.py
-
-
-
 
     """
 
@@ -123,58 +186,12 @@ def handle_options(args):
     return fits_files
 
 
-def check_tile(inpath=None, debug=False, fast=False,
-    ncheckfiles=None, clean=False):
-    """
-
-    """
-
-    import shutil
-
-    if debug: print('Processing:', inpath)
-    debug = False
-    filelist = glob.glob(inpath + '/*fit*')
-    nfiles = len(filelist)
-    print('Number of fits files:', nfiles)
-
-    if fast and clean and nfiles != ncheckfiles and nfiles != 0:
-        reply = raw_input("Delete directory "+ inpath +"? [y/[n]] ")
-        if reply == 'y' or reply == 'Y':
-            shutil.rmtree(inpath)
-
-        filelist = glob.glob(inpath + '/*fit*')
-        nfiles = len(filelist)
-        print('Number of fits files:', nfiles)
-
-    if not fast:
-        for infile in filelist:
-            if debug: print(infile)
-
-            hdulist = fits.open(infile)
-            nhdu = len(hdulist)
-            if debug: print('Number of extension:', nhdu)
-
-            for ihdu in xrange(0, nhdu):
-                if hdulist[ihdu].verify() is not None:
-                   print(ihdu, hdulist[ihdu].verify())
-
-    if fast and nfiles != ncheckfiles:
-        for infile in filelist:
-            if debug: print(infile)
-
-            hdulist = fits.open(infile)
-            nhdu = len(hdulist)
-            if debug: print('Number of extension:', nhdu)
-
-            for ihdu in xrange(0, nhdu):
-                if hdulist[ihdu].verify() is not None:
-                   print(ihdu, hdulist[ihdu].verify())
-
-    return nfiles
-
 
 if __name__ == '__main__':
     """
+
+    TODO: add a logfile for incomplete tiles that could
+    then use used to drive a rerun.
 
     """
     import os
@@ -183,7 +200,10 @@ if __name__ == '__main__':
 
     import argparse
 
-    description = "Parse command line arguements"
+    description = "Check the status of output files. \n" \
+                  + "Note the clean, fast, count options need some \n" \
+                  + "further consistency work"
+
     parser = argparse.ArgumentParser(
         description=description,
         formatter_class=argparse.ArgumentDefaultsHelpFormatter)
@@ -195,6 +215,10 @@ if __name__ == '__main__':
     parser.add_argument(
         '--fast', action='store_true', default=False,
         help='Fast option than checks number of files and only verifies the final output file')
+
+    parser.add_argument(
+        '--count', action='store_true', default=False,
+        help='Fast option using just filecount')
 
     parser.add_argument(
         '--nfiles', action='store', default=13,
@@ -211,6 +235,7 @@ if __name__ == '__main__':
     args = parser.parse_args()
 
     clean = args.clean
+    count = args.count
     debug = args.debug
     fast = args.fast
     ncheckfiles = args.nfiles
@@ -227,11 +252,19 @@ if __name__ == '__main__':
         pathlist = pathlist[iskip:]
         itile = iskip
 
+    npass = 0
+    nfail = 0
     for path in pathlist:
         itile = itile + 1
         print('Processing:', itile, path)
-        nfiles = check_tile(inpath=path, fast=fast,
+        nfiles = check_tile(inpath=path, fast=fast, count=count,
             ncheckfiles=ncheckfiles, clean=clean)
+
+        if nfiles == ncheckfiles:
+            npass = npass + 1
+
+        if nfiles != ncheckfiles:
+            nfail = nfail + 1
 
         if debug:
             print(path.split('/'))
@@ -239,18 +272,29 @@ if __name__ == '__main__':
         if debug:
             print('tile:', tile)
 
-        if nfiles > 0:
+        # option to check that the final output file is valid
+        # duplicates the check within check_tile
+        if nfiles > 0 and not count:
             filename_suffix = '_WISEfp_DEScat_WISE_match'
 
             infile = inpath + tile + '/' + tile + filename_suffix + '.fits'
-            print('Read:', infile)
 
-            hdulist = fits.open(infile)
-            nhdu = len(hdulist)
-            if debug: print('Number of extension:', nhdu)
+            if not os.path.exists(infile):
+                print('WARNING:', infile, 'does not exist')
 
-            for ihdu in xrange(0, nhdu):
-                if hdulist[ihdu].verify() is not None:
-                    print(ihdu, hdulist[ihdu].verify())
+            if os.path.exists(infile):
+                print('Verify:', infile)
+                hdulist = fits.open(infile)
+                nhdu = len(hdulist)
+                if debug:
+                    print('Number of extension:', nhdu)
 
-    filename = 'DES2327-5248.completed'
+                for ihdu in xrange(0, nhdu):
+                    if hdulist[ihdu].verify() is not None:
+                        print(ihdu, hdulist[ihdu].verify())
+
+      print()
+
+    print('Total: ', npass + nfail)
+    print('Passed:', npass)
+    print('Failed:', nfail)
